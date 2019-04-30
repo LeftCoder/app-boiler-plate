6b91e561e68824b592c5a11ecf8bb8bf06dfb41a
Kamil Graczyk <k.d.graczyk@gmail.com>
2019-04-30 23:35:15 +0200

    Playing around with loader and webpack-config

---
+++ src/main.js
@@ -0,0 +1,9 @@
+//import all js components
+import User from './components/User';
+
+//do fun stuff here
+const user = new User('Kamil');
+
+const element = document.createElement('h1');
+element.textContent = user.greet();
+document.body.appendChild(element);
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,62 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const devMode = process.env.NODE_ENV !== "production";
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: devMode ? "development" : "production",
-  entry: "./src/app/app.js",
-  output: {
-    filename: "./js/main.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: devMode ? "./css/[name].css" : "./css/[name].[hash].css"
-    })
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    compress: true,
-    port: 9000,
-    open: true
-  }
+module.exports = (env, options) => {
+  const devMode = options.mode !== 'production';
+
+  return {
+    entry: {
+      app: ['./src/main.js', './src/scss/main.scss'],
+    },
+    output: {
+      filename: devMode ? './js/[name].js' : './js/[name].min.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+            },
+          ],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+        filename: './index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: './css/[name].[hash].css',
+      }),
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000,
+      open: true,
+    },
+  };
 };
